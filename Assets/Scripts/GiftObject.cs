27d2283645c3c30b54f24a32e7b3fb94d6e8a84e
Xwilarg <xwilarg@yahoo.fr>
2018-12-22 18:43:32 +0100

    Add GiftObject script

---
+++ Assets/Scripts/GiftObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GiftObject : MonoBehaviour
+{
+    public enum GObject
+    {
+        Coal,
+        SegaNeptune,
+        PlasticDuck,
+        WhitePencil,
+        SubmachineGun,
+        LittleSister,
+        VeryBigCat,
+        MassiveDong,
+        Friends,
+        HentaiHeaven,
+        AppleJuice,
+        HumanSizeCockroach,
+        MeetingLeandre,
+        LesMiserables,
+        YellowVest,
+        Dakimakura
+    }
+
+    public GObject Obj;
+}
