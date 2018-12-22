e0a4faafce8c2586daf24076167d6f2d22ab207c
Xwilarg <xwilarg@yahoo.fr>
2018-12-22 01:48:01 +0100

    Add wish list

---
+++ Assets/Scripts/Wishlist.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wishlist : MonoBehaviour
+{
+    [SerializeField]
+    private Text wishList;
+
+    private const int listSizeMin = 3;
+    private const int listSizeMax = 10;
+
+    private readonly string[] wishes = new string[]
+    {
+        "Sega Neptune",
+        "Mommy's Plastic Duck",
+        "Submachine gun",
+        "White pencil",
+        "A little sister",
+        "A very big cat",
+        "A massive dong",
+        "Friends",
+        "Hentai Heaven to be back",
+        "Apple juice",
+        "A human size cockroach",
+        "Meeting Leandre in real life",
+        "Les Misérables complete edition"
+    };
+
+    private void Start()
+    {
+        List<int> wishesId = new List<int>();
+        int nbWishes = Random.Range(listSizeMin, listSizeMax);
+        for (int i = 0; i < nbWishes; )
+        {
+            int id = Random.Range(0, wishes.Length - 1);
+            if (!wishesId.Contains(id))
+            {
+                wishesId.Add(id);
+                i++;
+            }
+        }
+        string finalText = "";
+        int y = 1;
+        foreach (int i in wishesId)
+            finalText += (y++) + ". " + wishes[i] + System.Environment.NewLine;
+        wishList.text = finalText;
+    }
+}
