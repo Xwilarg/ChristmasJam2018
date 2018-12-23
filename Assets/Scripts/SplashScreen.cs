d54d2a504334282af19f8ca4aa67b234eb204eb2
Xwilarg <xwilarg@yahoo.fr>
2018-12-23 19:07:23 +0100

    Add Splash Screen

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    private const float refTimer = 8f;
+
+    private float timer;
+
+    private void Start()
+    {
+        timer = refTimer;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0f || Input.anyKeyDown)
+            SceneManager.LoadScene("MainMenu");
+    }
+}
