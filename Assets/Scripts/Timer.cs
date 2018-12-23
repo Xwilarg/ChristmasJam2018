5566e3f1693586b3c440d5692861c2476dc3ea9c
Xwilarg <xwilarg@yahoo.fr>
2018-12-23 17:07:30 +0100

    Add timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private const float maxTimer = 5f * 60f;
+    private float currTime;
+
+    public int finalScore { private set; get; }
+
+    private Text timerText;
+
+    private void Start()
+    {
+        currTime = maxTimer;
+        timerText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        currTime -= Time.deltaTime;
+        if (currTime < 0f)
+        {
+        }
+        timerText.text = "Remaining time: " + (int)(currTime / 60) + ":" + FormatNb((int)(currTime % 60));
+    }
+
+    private string FormatNb(int nb)
+    {
+        if (nb > 9)
+            return (nb.ToString());
+        return ("0" + nb);
+    }
+}
