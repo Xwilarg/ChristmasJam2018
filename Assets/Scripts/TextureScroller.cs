51f656568a901d740ed76ed08e314140f3f253ee
unknown <michael.feldman09@gmail.com>
2018-12-23 13:51:28 -0500

    Added a light ray effect to the empty box

---
+++ Assets/Scripts/TextureScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroller : MonoBehaviour
+{
+    public float flowRate;
+    private Vector2 flowVector;
+
+    private MeshRenderer myMesh;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMesh = transform.GetComponent<MeshRenderer>();
+        flowVector = new Vector2(flowRate, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMesh.material.mainTextureOffset += flowVector * Time.deltaTime;           
+    }
+}
